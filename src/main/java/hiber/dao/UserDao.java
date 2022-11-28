d51cc61987b4de1eb9ec6f9a1b96770de3fb819e
Nicholas2208 <nikolaj.belyakov.84@gmail>
2022-11-28 15:56:51 +0300

    remove throws statement

--- src/main/java/hiber/dao/UserDao.java
+++ src/main/java/hiber/dao/UserDao.java
@@ -9,5 +9,5 @@ public interface UserDao {
     void add(User user);
     List<User> getUsers();
 
-    User getUserByCar(String model, int series) throws NoResultException;
+    User getUserByCar(String model, int series);
 }
